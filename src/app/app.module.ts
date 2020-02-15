97a5fc3a91ed75302634fc8fa527ad3cae896a62
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:55:13 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { ActivityPanelComponent } from './activityPanel';
+import { ExampleComponent } from './example';
+import { OnePersonComponent } from './onePerson';
+import { PersonListComponent } from './personList';
+
+export const firebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>',
+
+  databaseURL: 'https://a2-fb-demo.firebaseio.com/'
+};
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  declarations: [
+    ExampleComponent,
+    ActivityPanelComponent,
+    PersonListComponent,
+    OnePersonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [ExampleComponent]
 })
 export class AppModule {}
