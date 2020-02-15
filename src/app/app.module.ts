83c4150c262b652ed994feaf2b9f68aa75c8df68
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:44 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {
+  BlueWrapperComponent,
+  BlueWrapperDirective
+} from './blue-wrapper.component';
+import { GreenWrapperComponent } from './green-wrapper.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    GreenWrapperComponent,
+    BlueWrapperComponent,
+    BlueWrapperDirective
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
