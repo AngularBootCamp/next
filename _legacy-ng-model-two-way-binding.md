f90405d370a34c5528142cd461943722a47cdda4
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:55:24 -0600

    curriculum update

---
+++ _legacy-ng-model-two-way-binding.md
@@ -0,0 +1,4 @@
+The source code for "legacy-ng-model-two-way-binding" is published online at:
+
+https://github.com/AngularBootCamp/legacy-ng-model-two-way-binding
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { BindingComponent } from './binding';
+
+// FormsModule contains NgModel, and is necessary for NgModel to work.
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  declarations: [BindingComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [BindingComponent]
 })
 export class AppModule {}
