e7ad096b0f3e12c60ea8353cf9ca5b635cc23587
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:51:21 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// BrowserModule includes a kit of "built in" browser-ready features.
+// It avoids the need to import specific features: *ngIf, *ngFor
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
