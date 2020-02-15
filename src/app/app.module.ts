bbeee2476815b032b5fe6c99f7c8169868eb3f89
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:55 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeGridComponent } from './employee-grid.component';
+import { EmployeeListComponent } from './employee-list.component';
+import { EmployeeComponent } from './employee.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeeGridComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
