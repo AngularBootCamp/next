d46be329fc1dc670678ae73d4cca43a394f7b5ae
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:55:09 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { SecondsToDatePipe } from './secondsToDatePipe';
+import { WeatherPanelComponent } from './weatherPanel';
+import { WeatherScreenComponent } from './weatherScreen';
+
+export const firebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>',
+
+  databaseURL: 'https://publicdata-weather.firebaseio.com/'
+};
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  declarations: [
+    WeatherScreenComponent,
+    WeatherPanelComponent,
+    SecondsToDatePipe
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [WeatherScreenComponent]
 })
 export class AppModule {}
