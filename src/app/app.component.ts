250e89305df00e5f96cd0e7df0130f0225496003
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:24 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { EmployeesService } from './employees.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <h3>Employees</h3>
+    <ul>
+      <li *ngFor="let e of employees | async">
+        {{ e.first_name }} {{ e.last_name }}
+      </li>
+    </ul>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  employees: Observable<any[]>;
+
+  constructor(es: EmployeesService) {
+    // this.employees = es.getEmployees();
+    // this.employees = es.poll1();
+    this.employees = es.poll2();
+  }
+}
