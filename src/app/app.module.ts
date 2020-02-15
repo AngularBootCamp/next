fce968928824a9004c7f93f22069a01e8a58ef6c
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:12 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,42 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {
+  BlueBoxComponent,
+  ChatComponent,
+  HomeComponent,
+  NameComponent,
+  TaskComponent
+} from './components';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'hello/:name', component: NameComponent },
+  { path: 'blue', component: BlueBoxComponent },
+
+  { path: 'chat', component: ChatComponent, outlet: 'side' },
+  { path: 'task', component: TaskComponent, outlet: 'side' }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NameComponent,
+    BlueBoxComponent,
+    ChatComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
