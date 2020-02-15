de5c8e9811a4082759d24203ddb78e2acca1fa21
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:32 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DefaultStrategyComponent } from './display/default-strategy.component';
+import { ItemDetailComponent } from './display/item-detail.component';
+import { OnPushStrategyComponent } from './display/onpush-strategy.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    DefaultStrategyComponent,
+    OnPushStrategyComponent,
+    ItemDetailComponent
+  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
