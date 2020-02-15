f1bb81a97f55401b4ee68d6b155e5af9e5201e9f
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:50:53 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+// The module class can have any name you like, but it is wise to name
+// it ___Module, because the TypeScript type system cannot yet express
+// a type for "the class must have decorator Abc".
 
 @NgModule({
   declarations: [AppComponent],
