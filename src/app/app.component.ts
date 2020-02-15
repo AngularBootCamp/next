08e75cedefe4ef5aeef6f01ea27feb05175182b2
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:54:26 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  onNotify(notification: string) {
+    window.alert(notification);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ScoreComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
