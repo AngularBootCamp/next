ea25dd83ae8ddad848d0460b86b99306c9a2c36a
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:52:45 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { CounterDisplayComponent } from './counter-display.component';
+import { ROOT_REDUCERS } from './reducers';
+import { AppState } from './state';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, CounterDisplayComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot<AppState>(ROOT_REDUCERS)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
