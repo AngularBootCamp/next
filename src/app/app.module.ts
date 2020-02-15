f3e234164833f9285bdc3b26139a4b265adc194a
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:52:37 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
-// STOP! - We will talk about this later
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
