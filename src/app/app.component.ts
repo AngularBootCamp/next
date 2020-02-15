660a093fc8780c5b9259da1be4201899c7fb858d
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:51:05 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <h4>Example application with multiple modules</h4>
+    <payroll-search></payroll-search>
+    <hr-files-search></hr-files-search>
+  `
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HrModule } from './hr-files/hr.module';
+import { PayrollModule } from './payroll/payroll.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [PayrollModule, HrModule, BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
