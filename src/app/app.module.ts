3ac629ce4a190f4cb1858b0f293a37a5bdf1bf95
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:54:30 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first.component';
+import { SecondComponent } from './second.component';
+import { ThirdComponent } from './third.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
