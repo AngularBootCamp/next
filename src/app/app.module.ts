b01bb7083337f90800506fb9b7bb267830d637ca
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:52:41 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './notification-manager/employee-list/employee-list.component';
+import { NotificationManagerComponent } from './notification-manager/notification-manager.component';
+import { PositionListComponent } from './notification-manager/position-list/position-list.component';
+import { SharedModule } from './shared-module/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    NotificationManagerComponent,
+    EmployeeListComponent,
+    PositionListComponent,
+    AppComponent
+  ],
+  imports: [BrowserModule, SharedModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
