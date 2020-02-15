0fcaec5c6a8ec64bb1d8af7d32de346dcc92d2d8
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:53:28 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-// STOP! - We will talk about this later
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManagerComponent } from './manager.component';
+import { WorkerComponent } from './worker.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ManagerComponent, WorkerComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
