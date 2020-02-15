617a73c489b3f792685ed7847a3a1797c85ec44b
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:55:01 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+// import { CarStateService } from './car-state.service';
+
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // ,providers: [CarStateService]
 })
-export class AppComponent {}
+export class AppComponent {
+  cars: any[] = [];
+
+  addOne() {
+    this.cars.push({});
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-// STOP! - We will talk about this later
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AxleSelectorComponent } from './axle-selector/axle-selector.component';
+import { CarOrderComponent } from './car-order/car-order.component';
+import { ValidityDisplayComponent } from './validity-display/validity-display.component';
+import { WheelSelectorComponent } from './wheel-selector/wheel-selector.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CarOrderComponent,
+    AxleSelectorComponent,
+    WheelSelectorComponent,
+    ValidityDisplayComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
