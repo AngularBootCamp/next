b684535007b1c564822a6a27d0c128edef5ca96d
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:54:15 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { HelloService } from './hello.service';
+
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  greeting = '';
+
+  constructor(private hello: HelloService) {}
+
+  calculateGreeting() {
+    this.greeting = this.hello.calculateHello('Hello');
+  }
+}
