0d89c34d00bf3310c02107f49fe4174903499605
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:54:37 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <p>
+      This step is about Upgrading from AngularJS 1.x; there is no
+      code here, see instruction presentation and discussion.
+    </p>
+  `
 })
 export class AppComponent {}
