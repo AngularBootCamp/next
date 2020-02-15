b5933f4eac03d48ba6160cd37e38f6d674a14596
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:55:35 -0600

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-// STOP! - We will talk about this later
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RedditSearchComponent } from './reddit-search/reddit-search.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, RedditSearchComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
