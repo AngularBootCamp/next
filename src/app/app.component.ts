9178fa9caf5f7889829c1bd916d18df8282af75f
Kyle Cordes <kyle.cordes@oasisdigital.com>
2020-02-14 23:52:20 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+// import { Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  // constructor(router: Router) {
+  //   router.events.subscribe((event: Event) =>
+  //     console.log('router event!', event));
+  // }
+}
